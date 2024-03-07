e821a127b912502c89f4abbd5429a949783c9740
Aqsa Gull <aqsabravegirl3@gmail.com>
2024-03-07 15:33:05 +0500

    first commit

---
+++ operators.js
@@ -0,0 +1,467 @@
+"use strict";
+// Arithemetic oparotors 
+// Additional  (+)
+// printing method one
+console.log(2 + 3);
+// // // printing method two
+const num1 = 5;
+const num2 = 5;
+console.log(num1 + num2);
+// // // printing method three
+const num3 = 2;
+const num4 = 4;
+const sum1 = num3 + num4 + 2;
+console.log(sum1);
+// Additional with data type string  (+)
+const firstName1 = "I am Aqsa";
+const secondName2 = "Gull";
+console.log(firstName1 + " " + secondName2);
+const firstMassage = "How are you aqsa?";
+const secondMassage = "I am fine what about you?";
+console.log(firstMassage + "  " + secondMassage);
+const profession1 = "What do you do?";
+const profession = "I am Web developer.. ";
+console.log(profession1 + "  " + profession);
+// Subtraction  (-)
+// printing method one
+console.log(5 - 3);
+// // // printing method two
+const num5 = 5;
+const num6 = 5;
+console.log(num5 - num6);
+// // printing method three
+const num7 = 2;
+const num8 = 4;
+const sum2 = num7 + num8 - 2;
+console.log(sum2);
+// Multipilication  (*)
+// printing method one
+console.log(5 * 3);
+// // printing method two
+const num9 = 5;
+const num10 = 5;
+console.log(num9 * num10);
+// // printing method three
+const num11 = 2;
+const num12 = 4;
+const sum3 = num11 * num12 + 2;
+console.log(sum3);
+// division  (/)
+// printing method one
+console.log(5 / 3);
+// // printing method two
+const num13 = 5;
+const num14 = 5;
+console.log(num13 / num14);
+// // printing method three
+const num15 = 2;
+const num16 = 4;
+const sum4 = num15 / num16 + 2;
+console.log(sum4);
+// Modulus / Remaindar   (%)
+// printing method one
+console.log(5 % 3); // Remaindar 
+// // printing method two
+const num17 = 5;
+const num18 = 5;
+console.log(num17 % num18); // Remaindar 
+// // printing method three
+const num19 = 2;
+const num20 = 4;
+const sum5 = num19 % num20 / 2;
+console.log(sum5); // Remaindar 
+// // Exponentiation   (**)
+// // printing method one
+console.log(5 ** 3);
+// // printing method two
+const num21 = 5;
+const num22 = 2;
+console.log(num21 ** num22);
+// // printing method three
+const num23 = 2;
+const num24 = 4;
+const sum6 = num23 ** num24;
+console.log(sum6);
+//  Increament   (++)
+// printing method one
+let x = 5;
+++x;
+console.log(x);
+// // printing method two
+let a = 10;
+let b = ++a;
+console.log(a);
+// //  Decrement   (--)
+// // printing method one
+let y = 5;
+--y;
+console.log(y);
+// // printing method two
+let c = 10;
+let d = --c;
+console.log(c);
+// prefix increament
+let p, p1 = 6;
+p = ++p1;
+console.log(p);
+console.log(p1);
+let q, q1 = 5;
+q = ++q1;
+console.log(q);
+console.log(q1);
+// prefix decreament
+let r, r1 = 6;
+r = --r1;
+console.log(r);
+console.log(r1);
+let s, s1 = 5;
+s = --s1;
+console.log(s);
+console.log(s1);
+// postfix increament
+let t, t1 = 2;
+t = t1++;
+console.log(t);
+console.log(t1);
+let u, u1 = 6;
+u = u1++;
+console.log(u);
+console.log(u1);
+// postfix decreament
+let v, v1 = 2;
+v = v1--;
+console.log(v);
+console.log(v1);
+let w, w1 = 6;
+w = w1--;
+console.log(w);
+console.log(w1);
+// Assigment operators
+// Additional equal operator (+=)
+// first method
+let o1 = 8;
+o1 += 3;
+console.log(o1);
+let o2 = 5;
+o2 += 5;
+console.log(o2);
+let o3 = 20;
+o3 += 20;
+console.log(o3);
+// second method
+let o4 = 12;
+let o5 = 13;
+let sum7 = o4 += o5;
+console.log(sum7);
+let o6 = 12;
+let o7 = 12;
+let sum8 = o6 += o7;
+console.log(sum8);
+let o8 = 10;
+let o9 = 20;
+let sum9 = o8 += o9;
+console.log(sum9);
+// subtraction equal operator (-=)
+// first method
+let o10 = 8;
+o10 -= 3;
+console.log(o10);
+let o11 = 5;
+o11 -= 5;
+console.log(o11);
+let o12 = 20;
+o12 -= 15;
+console.log(o12);
+// second method
+let o13 = 15;
+let o14 = 13;
+let sum10 = o13 -= o14;
+console.log(sum10);
+let o15 = 12;
+let o16 = 11;
+let sum11 = o15 -= o16;
+console.log(sum11);
+let o17 = 10;
+let o18 = 1;
+let sum12 = o17 -= o18;
+console.log(sum12);
+// devisi0n equal operator (/=)
+// first method
+let o19 = 2;
+o19 /= 3;
+console.log(o19);
+let o20 = 4;
+o20 /= 5;
+console.log(o20);
+let o21 = 2;
+o21 /= 7;
+console.log(o21);
+// second method
+let o22 = 15;
+let o23 = 13;
+let sum13 = o22 /= o23;
+console.log(sum13);
+let o24 = 12;
+let o25 = 11;
+let sum14 = o24 /= o25;
+console.log(sum14);
+let o26 = 10;
+let o27 = 1;
+let sum15 = o26 /= o27;
+console.log(sum15);
+// multipiliction equal operator (*=)
+// first method
+let o28 = 2;
+o28 *= 3;
+console.log(o28);
+let o29 = 4;
+o29 *= 5;
+console.log(o29);
+let o30 = 2;
+o30 *= 7;
+console.log(o30);
+// second method
+let o31 = 5;
+let o32 = 3;
+let sum16 = o31 *= o32;
+console.log(sum16);
+let o33 = 2;
+let o34 = 10;
+let sum17 = o33 *= o34;
+console.log(sum17);
+let o35 = 10;
+let o36 = 1;
+let sum18 = o35 *= o36;
+console.log(sum18);
+// // Modulus / Remaindar  equal operator (%=)
+// // first method
+let o37 = 2;
+o37 %= 3;
+console.log(o37);
+let o38 = 4;
+o38 %= 5;
+console.log(o38);
+let o39 = 2;
+o39 %= 7;
+console.log(o39);
+// // second method
+let o41 = 5;
+let o42 = 3;
+let sum19 = o41 %= o42;
+console.log(sum19);
+let o43 = 2;
+let o44 = 10;
+let sum20 = o43 %= o44;
+console.log(sum20);
+let o45 = 10;
+let o46 = 1;
+let sum21 = o45 %= o46;
+console.log(sum21);
+// Exponentiation  equal operator (**=)
+// // first method
+let o47 = 2;
+o47 **= 0;
+console.log(o47);
+let o48 = 4;
+o48 **= 2;
+console.log(o48);
+let o49 = 2;
+o49 **= 1;
+console.log(o49);
+// // second method
+let o50 = 5;
+let o51 = 2;
+let sum22 = o50 **= o51;
+console.log(sum22);
+let o52 = 2;
+let o53 = 1;
+let sum23 = o52 **= o53;
+console.log(sum23);
+let o54 = 1;
+let o55 = 1;
+let sum24 = o54 **= o55;
+console.log(sum24);
+// Comparision operators 
+// Greater than (>)
+//  one method
+let e = 5;
+let f = 5;
+console.log(e > f); //false
+let e1 = 5;
+let f1 = 4;
+console.log(e1 > f1); // true
+let e2 = 5;
+let f2 = 9;
+console.log(e2 > f2); // false
+let e3 = 15;
+let f3 = 9;
+console.log(e3 > f3); // true
+//  Two method
+let e4 = 25;
+let f4 = 20;
+let g1 = e4 > f4;
+console.log(e4 > f4); // true
+let e5 = 20;
+let f5 = 20;
+let g2 = e5 > f5;
+console.log(e5 > f5); // false
+let e6 = 20;
+let f6 = 10;
+let g3 = e6 > f6;
+console.log(e6 > f6); // true
+// Three method
+console.log(5 > 7); // false
+console.log(10 > 7); // true
+console.log(10 > 10); // false
+// Less than (<)
+//  one method
+let g = 4;
+let h = 6;
+console.log(g < h); //true
+let g10 = 42;
+let h1 = 41;
+console.log(g10 < h1); // false
+let g20 = 5;
+let h2 = 10;
+console.log(g20 < h2); // true
+let g30 = 15;
+let h3 = 9;
+console.log(g30 < h3); // false
+//  Two method
+let g4 = 15;
+let h4 = 20;
+let i4 = g4 < h4;
+console.log(i4); // true
+let g5 = 20;
+let h5 = 20;
+let i2 = g5 < h5;
+console.log(i2); // false
+let g6 = 2;
+let h6 = 10;
+let i3 = g6 < h6;
+console.log(i3); // true
+// Three method
+console.log(5 < 7); // true
+console.log(10 < 7); // false  
+console.log(10 > 10); // false
+// Greater than with equal to (>=)
+//  one method
+let j = 7;
+let k = 6;
+console.log(j >= k); //true
+let j1 = 42;
+let k1 = 41;
+console.log(j1 >= k1); //true
+let j2 = 5;
+let k2 = 10;
+console.log(j2 >= k2); // false
+let j3 = 15;
+let k3 = 9;
+console.log(j3 >= k3); // true
+//  Two method
+let j4 = 15;
+let k4 = 20;
+let l1 = j4 >= k4;
+console.log(l1); // false
+let j5 = 20;
+let k5 = 22;
+let l2 = j5 >= k5;
+console.log(l2); // false
+let j6 = 20;
+let k6 = 10;
+let l3 = j6 >= k6;
+console.log(l3); // true
+// Three method
+console.log(5 >= 7); // false
+console.log(10 >= 7); // true 
+console.log(10 >= 10); // true
+// four method
+let m = 5;
+m >= 3;
+console.log(5 >= 3); // true
+let n = 2;
+n >= 3;
+console.log(2 >= 3); // false
+// Less than with equal to (<=)
+//  one method
+let j9 = 4;
+let k9 = 6;
+console.log(j9 <= k9); //true
+let j10 = 42;
+let k10 = 41;
+console.log(j10 <= k10); // false
+let j20 = 5;
+let k20 = 10;
+console.log(j20 <= k20); // true
+let j30 = 15;
+let k30 = 9;
+console.log(j30 <= k30); // false
+//  Two method
+let j40 = 15;
+let k40 = 20;
+let l10 = j40 <= k40;
+console.log(l10); // true
+let j50 = 20;
+let k50 = 22;
+let l20 = j50 <= k50;
+console.log(l20); // true
+let j60 = 20;
+let k60 = 10;
+let l30 = j60 <= k60;
+console.log(l30); // false
+// Three method
+console.log(5 <= 7); // true
+console.log(10 <= 7); // false 
+console.log(10 <= 10); // true
+// four method
+let m1 = 5;
+m <= 3;
+console.log(5 <= 3); // false
+let n1 = 2;
+n <= 3;
+console.log(2 <= 3); // true
+// Double equal to (==)
+// one method 
+let a1 = 5;
+let b1 = 5;
+let c1 = a1 == b1;
+console.log(c1); // true
+let a2 = 8;
+let b2 = 5;
+let c2 = a2 == b2;
+console.log(c2); // false
+let a3 = 8;
+let b3 = 12;
+let c3 = a3 == b3;
+console.log(c3); // false
+// // Tripple equal to (===)
+let z = 5;
+let y4 = "9";
+console.log(z === y4); // false
+let z1 = 5;
+let y1 = 5;
+console.log(z1 === y1); // true
+let z2 = 6;
+let y2 = 6;
+console.log(z2 === y2); // true
+// Not equal operator  (!=)
+let w11 = 5;
+let v11 = 7;
+console.log(w11 != v11); // true
+let w10 = 5;
+let v10 = 5;
+console.log(w10 != v10); // false
+let w2 = 9;
+let v2 = 7;
+console.log(w2 != v2); // true
+// Not equal to with double equal (!==)
+let w3 = 9;
+let v3 = 9;
+console.log(w3 !== v3); // false
+let w4 = 9;
+let v4 = "9";
+console.log(w4 !== v4); // true
+let w5 = 9;
+let v5 = "9";
+// // let x5:boolean=true
+console.log(w5 !== v5); // true
---
+++ operators.ts
@@ -0,0 +1,921 @@
+// Arithemetic oparotors 
+
+// Additional  (+)
+
+// printing method one
+
+console.log(2 + 3)
+
+// // // printing method two
+
+const num1:number=5
+const num2:number=5
+console.log(num1 + num2)
+
+// // // printing method three
+
+const num3:number =2
+const num4:number =4
+const sum1:number= num3 +num4 + 2
+
+console.log(sum1)
+
+// Additional with data type string  (+)
+
+
+const firstName1:string="I am Aqsa"
+const secondName2:string="Gull"
+console.log(firstName1 + " " + secondName2 )
+
+
+const firstMassage:string="How are you aqsa?"
+const secondMassage:string="I am fine what about you?"
+console.log(firstMassage + "  " + secondMassage )
+
+
+const profession1:string="What do you do?"
+const profession:string="I am Web developer.. "
+console.log(profession1 + "  " + profession)
+
+
+
+// Subtraction  (-)
+
+// printing method one
+
+console.log(5 - 3)
+
+// // // printing method two
+
+const num5:number=5
+const num6:number=5
+console.log(num5 - num6)
+
+// // printing method three
+
+const num7:number =2
+const num8:number =4
+const sum2:number= num7 + num8 - 2
+
+console.log(sum2)
+
+// Multipilication  (*)
+
+// printing method one
+
+console.log(5 * 3)
+
+// // printing method two
+
+const num9:number=5
+const num10:number=5
+console.log(num9 * num10)
+
+// // printing method three
+
+const num11:number =2
+const num12:number =4
+const sum3:number= num11 * num12 + 2
+
+console.log(sum3)
+
+// division  (/)
+
+// printing method one
+
+console.log(5 / 3)
+
+// // printing method two
+
+const num13:number=5
+const num14:number=5
+console.log(num13 / num14)
+
+// // printing method three
+
+const num15:number =2
+const num16:number =4
+const sum4:number= num15 / num16 + 2
+
+console.log(sum4)
+
+// Modulus / Remaindar   (%)
+
+// printing method one
+
+console.log(5 % 3) // Remaindar 
+
+// // printing method two
+
+const num17:number=5
+const num18:number=5
+console.log(num17 % num18) // Remaindar 
+
+// // printing method three
+
+const num19:number =2
+const num20:number =4
+const sum5:number= num19 % num20 / 2
+
+console.log(sum5) // Remaindar 
+
+
+// // Exponentiation   (**)
+
+// // printing method one
+
+console.log(5 ** 3) 
+
+// // printing method two
+
+const num21:number=5
+const num22:number=2
+console.log(num21 ** num22) 
+
+// // printing method three
+
+const num23:number =2
+const num24:number =4
+const sum6:number= num23 ** num24 
+
+console.log(sum6) 
+
+
+//  Increament   (++)
+
+// printing method one
+
+let x:number=5;
+++x;
+console.log(x) 
+
+// // printing method two
+
+let a:number=10;
+let b:number=++a
+console.log(a)
+
+// //  Decrement   (--)
+
+// // printing method one
+
+let y:number=5;
+--y;
+console.log(y) 
+
+// // printing method two
+
+let c:number=10;
+let d:number=--c
+console.log(c)
+
+
+// prefix increament
+
+let p:number,p1:number= 6
+p=++p1
+console.log(p)
+console.log(p1)
+
+
+let q:number,q1:number= 5
+q=++q1
+console.log(q)
+console.log(q1)
+
+// prefix decreament
+
+let r:number,r1:number= 6
+r=--r1
+console.log(r)
+console.log(r1)
+
+
+let s:number,s1:number= 5
+s=--s1
+console.log(s)
+console.log(s1)
+
+
+// postfix increament
+
+
+let t:number,t1:number= 2
+t=t1++
+console.log(t)
+console.log(t1)
+
+
+let u:number,u1:number= 6
+u=u1++
+console.log(u)
+console.log(u1)
+
+
+// postfix decreament
+
+let v:number,v1:number= 2
+v=v1--
+console.log(v)
+console.log(v1)
+
+
+let w:number,w1:number= 6
+w=w1--
+console.log(w)
+console.log(w1)
+
+
+// Assigment operators
+
+// Additional equal operator (+=)
+
+// first method
+
+let o1:number=8
+o1+=3
+console.log(o1)
+
+let o2:number=5
+o2+=5
+console.log(o2)
+
+let o3:number=20
+o3+=20
+console.log(o3)
+
+// second method
+
+
+let o4:number =12
+let o5:number=13
+let sum7=o4 += o5
+
+console.log(sum7)
+
+
+let o6:number =12
+let o7:number=12
+let sum8=o6 += o7
+
+console.log(sum8)
+
+
+let o8:number =10
+let o9:number=20
+let sum9=o8 += o9
+
+console.log(sum9)
+
+// subtraction equal operator (-=)
+
+
+// first method
+
+let o10:number=8
+o10-=3
+console.log(o10)
+
+
+let o11:number=5
+o11-=5
+console.log(o11)
+
+
+
+let o12:number=20
+o12-=15
+console.log(o12)
+
+// second method
+
+
+let o13:number =15
+let o14:number=13
+let sum10=o13 -= o14
+
+console.log(sum10)
+
+
+let o15:number =12
+let o16:number=11
+let sum11=o15 -= o16
+
+console.log(sum11)
+
+
+let o17:number =10
+let o18:number=1
+let sum12=o17 -= o18
+
+console.log(sum12)
+
+// devisi0n equal operator (/=)
+
+
+// first method
+
+let o19:number=2
+o19/=3
+console.log(o19)
+
+
+let o20:number=4
+o20/=5
+console.log(o20)
+
+
+let o21:number=2
+o21/=7
+console.log(o21)
+
+// second method
+
+
+let o22:number =15
+let o23:number=13
+let sum13=o22 /= o23
+
+console.log(sum13)
+
+
+let o24:number =12
+let o25:number=11
+let sum14=o24 /= o25
+
+console.log(sum14)
+
+
+let o26:number =10
+let o27:number=1
+let sum15=o26 /= o27
+
+console.log(sum15)
+
+// multipiliction equal operator (*=)
+
+
+// first method
+
+let o28:number=2
+o28*=3
+console.log(o28)
+
+
+let o29:number=4
+o29*=5
+console.log(o29)
+
+
+
+let o30:number=2
+o30*=7
+console.log(o30)
+
+// second method
+
+
+let o31:number =5
+let o32:number=3
+let sum16=o31 *= o32
+
+console.log(sum16)
+
+
+let o33:number =2
+let o34:number=10
+let sum17=o33 *= o34
+
+console.log(sum17)
+
+
+let o35:number =10
+let o36:number=1
+let sum18=o35 *= o36
+
+console.log(sum18)
+
+
+
+// // Modulus / Remaindar  equal operator (%=)
+
+
+// // first method
+
+let o37:number=2
+o37%=3
+console.log(o37)
+
+
+let o38:number=4
+o38%=5
+console.log(o38)
+
+
+let o39:number=2
+o39%=7
+console.log(o39)
+
+// // second method
+
+
+let o41:number =5
+let o42:number=3
+let sum19=o41 %= o42
+
+console.log(sum19)
+
+
+let o43:number =2
+let o44:number=10
+let sum20=o43 %= o44
+
+console.log(sum20)
+
+
+let o45:number =10
+let o46:number=1
+let sum21=o45 %= o46
+
+console.log(sum21)
+
+
+// Exponentiation  equal operator (**=)
+
+
+// // first method
+
+let o47:number=2
+o47**=0
+console.log(o47)
+
+
+let o48:number=4
+o48**=2
+console.log(o48)
+
+
+let o49:number=2
+o49**=1
+console.log(o49)
+
+// // second method
+
+let o50:number =5
+let o51:number=2
+let sum22=o50 **= o51
+
+console.log(sum22)
+
+
+let o52:number =2
+let o53:number=1
+let sum23=o52 **= o53
+
+console.log(sum23)
+
+
+let o54:number =1
+let o55:number=1
+let sum24=o54 **= o55
+
+console.log(sum24)
+
+
+// Comparision operators 
+
+// Greater than (>)
+
+//  one method
+
+let e:number=5
+let f:number=5 
+
+console.log(e>f)  //false
+
+
+let e1:number=5
+let f1:number=4
+
+console.log(e1 > f1)  // true
+    
+
+let e2:number=5
+let f2:number=9
+
+console.log(e2 > f2)  // false
+    
+
+let e3:number=15
+let f3:number=9
+
+console.log(e3 > f3)  // true
+   
+//  Two method
+
+let e4:number=25
+let f4:number=20
+let g1:boolean= e4 > f4
+
+console.log(e4 > f4)  // true
+
+let e5:number=20
+let f5:number=20
+let g2:boolean= e5 > f5
+
+console.log(e5 > f5)  // false
+    
+let e6:number=20
+let f6:number=10
+let g3:boolean= e6 > f6
+
+console.log(e6 > f6)  // true
+    
+// Three method
+
+console.log(5>7) // false
+
+console.log(10>7) // true
+
+console.log(10>10) // false
+
+
+// Less than (<)
+
+//  one method
+
+let g:number=4
+let h:number=6
+
+console.log(g<h)  //true
+
+
+let g10:number=42
+let h1:number=41
+
+console.log(g10 < h1)  // false
+    
+
+let g20:number=5
+let h2:number=10
+
+console.log(g20 < h2)  // true
+    
+
+let g30:number=15
+let h3:number=9
+
+console.log(g30 < h3)  // false
+   
+//  Two method
+
+let g4:number=15
+let h4:number=20
+let i4:boolean= g4 < h4
+
+console.log(i4)  // true
+
+let g5:number=20
+let h5:number=20
+let i2:boolean= g5 < h5
+
+console.log(i2)  // false
+    
+let g6:number=2
+let h6:number=10
+let i3:boolean= g6 < h6
+
+console.log(i3)  // true
+    
+// Three method
+
+console.log(5<7)  // true
+
+console.log(10<7) // false  
+
+console.log(10>10) // false
+
+
+// Greater than with equal to (>=)
+
+
+//  one method
+
+let j:number=7
+let k:number=6
+
+console.log(j>=k)  //true
+
+
+ let j1:number=42
+ let k1:number=41
+
+console.log(j1 >= k1)  //true
+
+
+let j2:number=5
+let k2:number=10
+
+console.log(j2 >= k2)  // false
+    
+
+let j3:number=15
+let k3:number=9
+
+console.log(j3 >= k3)  // true
+   
+//  Two method
+
+let j4:number=15
+let k4:number=20
+let l1:boolean= j4 >= k4
+
+console.log(l1)  // false
+
+let j5:number=20
+let k5:number=22
+let l2:boolean= j5 >= k5
+
+console.log(l2)  // false
+    
+let j6:number=20
+let k6:number=10
+let l3:boolean= j6 >= k6
+
+console.log(l3)  // true
+    
+// Three method
+
+console.log(5>=7)  // false
+
+console.log(10>=7) // true 
+
+console.log(10>=10) // true
+
+// four method
+
+
+let m=5
+ m>=3
+ console.log(5>=3) // true
+
+
+let n=2
+ n>=3
+ console.log(2>=3) // false
+
+
+ // Less than with equal to (<=)
+
+
+//  one method
+
+let j9:number=4
+let k9:number=6
+
+console.log(j9<=k9)  //true
+
+
+let j10:number=42
+let k10:number=41
+
+console.log(j10 <= k10)  // false
+    
+
+let j20:number=5
+let k20:number=10
+
+console.log(j20 <= k20)  // true
+    
+
+let j30:number=15
+let k30:number=9
+
+console.log(j30 <= k30)  // false
+   
+//  Two method
+
+let j40:number=15
+let k40:number=20
+let l10:boolean= j40 <= k40
+
+console.log(l10)  // true
+
+let j50:number=20
+let k50:number=22
+let l20:boolean= j50 <= k50
+
+console.log(l20)  // true
+    
+let j60:number=20
+let k60:number=10
+let l30:boolean= j60 <= k60
+
+console.log(l30)  // false
+    
+// Three method
+
+console.log(5<=7)  // true
+
+console.log(10<=7) // false 
+
+console.log(10<=10) // true
+
+// four method
+
+
+let m1=5
+ m<=3
+ console.log(5<=3) // false
+
+
+let n1=2
+ n<=3
+ console.log(2<=3) // true
+
+
+// Double equal to (==)
+
+// one method 
+
+let a1:number=5
+let b1:number=5
+let c1:boolean= a1==b1 
+console.log(c1)  // true
+
+
+let a2:number=8
+let b2:number=5
+let c2:boolean= a2==b2 
+console.log(c2)  // false
+
+let a3:number=8
+let b3:number=12
+let c3:boolean= a3==b3 
+console.log(c3)  // false
+
+
+
+// // Tripple equal to (===)
+
+
+
+  let z:number=5
+let y4:string="9"
+
+ 
+console.log(z===y4)  // false
+
+
+let z1:number=5
+let y1:number=5
+
+ 
+console.log(z1===y1)  // true
+
+
+let z2:number=6
+let y2:number=6
+
+ 
+console.log(z2===y2)  // true
+
+
+
+// Not equal operator  (!=)
+
+
+let w11:number=5
+let v11:number=7
+
+console.log(w11 != v11)   // true
+
+let w10=5
+let v10=5
+
+console.log(w10 != v10)   // false
+
+
+ 
+let w2=9
+let v2=7
+
+console.log(w2 != v2)   // true
+
+
+// Not equal to with double equal (!==)
+
+
+let w3:number=9
+let v3:number=9
+
+console.log(w3 !== v3)   // false
+
+
+let w4:number=9
+let v4:string="9"
+
+console.log(w4 !== v4)   // true
+
+
+let w5:number=9
+let v5:string="9"
+// // let x5:boolean=true
+console.log(w5 !== v5 , )   // true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
